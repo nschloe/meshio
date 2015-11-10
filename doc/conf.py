6d8ed22408b4054fd53ac4f6aec2370f7ff31800
Nico Schlömer <nico.schloemer@gmail.com>
2015-11-10 10:37:47 +0100

    doc: mock vtk

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return Mock()
 
-MOCK_MODULES = ['h5py']
+MOCK_MODULES = ['vtk']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
