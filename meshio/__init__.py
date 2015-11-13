6aece8e4fee31e9605805fcbb96dc1eadf471e90
Nico Schlömer <nico.schloemer@gmail.com>
2015-11-13 01:53:36 +0100

    version bump to 0.2.0

--- meshio/__init__.py
+++ meshio/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-__version__ = '0.1.4'
+__version__ = '0.2.0'
 __author__ = 'Nico Schlömer'
 __author_email__ = 'nico.schloemer@gmail.com'
 __website__ = 'https://github.com/nschloe/meshio'
