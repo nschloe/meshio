d3107f2e02fbba5a179af0b931f2fd751f33498a
Nico Schlömer <nico.schloemer@gmail.com>
2015-12-23 17:58:03 +0100

    dolfin: better error message

--- meshio/dolfin_io.py
+++ meshio/dolfin_io.py
@@ -92,7 +92,8 @@ def write(
         cell_type = 'triangle'
     else:
         raise RuntimeError(
-          'Dolfin XML can only deal with triangle or tetra.'
+          'Dolfin XML can only deal with triangle or tetra. The input data
+          contains ' + ', '.join(cells.keys())
           )
 
     if len(cells) > 1:
