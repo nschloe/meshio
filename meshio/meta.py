6bba6d4c53434e37fafc1ff6d06c425814b4b7b5
Nico Schlömer <nico.schloemer@gmail.com>
2016-12-06 20:31:33 +0100

    version bump

--- meshio/meta.py
+++ meshio/meta.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-__version__ = '1.4.1'
+__version__ = '1.4.2'
 __author__ = 'Nico Schlömer'
 __author_email__ = 'nico.schloemer@gmail.com'
 __website__ = 'https://github.com/nschloe/meshio'
