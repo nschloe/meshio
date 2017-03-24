90eb12b34d2b42b7b39b6980df38867934120d39
Nico Schlömer <nico.schloemer@gmail.com>
2017-03-24 14:15:00 +0100

    unicode fix

--- meshio/__about__.py
+++ meshio/__about__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 __version__ = '1.7.4'
-__author__ = 'Nico Schlömer'
+__author__ = u'Nico Schlömer'
 __author_email__ = 'nico.schloemer@gmail.com'
 __website__ = 'https://github.com/nschloe/meshio'
 __license__='License :: OSI Approved :: MIT License'
