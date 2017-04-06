52ddf0ae5d598d2484f0dab05dec3e48559d9f05
Nico Schlömer <nico.schloemer@gmail.com>
2017-04-06 12:50:57 +0200

    better packaging

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #
-import os
-from distutils.core import setup
 import codecs
+import os
+from setuptools import setup
 
 # https://packaging.python.org/single_source_version/
 base_dir = os.path.abspath(os.path.dirname(__file__))
