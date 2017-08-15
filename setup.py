cf995d4e86123a42006d599a42bf1fb023c954ed
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-15 16:15:32 +0200

    Python3 locale fix (#113)
    * open __about__ as bytes
    
    * version bump
    
    * test: don't force UTF8
    
    * update pipdate dependency
    
    * don't require pipdate

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 # https://packaging.python.org/single_source_version/
 base_dir = os.path.abspath(os.path.dirname(__file__))
 about = {}
-with open(os.path.join(base_dir, 'meshio', '__about__.py')) as f:
+with open(os.path.join(base_dir, 'meshio', '__about__.py'), 'rb') as f:
     exec(f.read(), about)
 
 
@@ -36,7 +36,7 @@ setup(
     platforms='any',
     install_requires=[
         'numpy',
-        'pipdated',
+        'pipdate',
         ],
     extras_require={
         'dolfin': ['lxml'],  # Dolfin's legacy format
