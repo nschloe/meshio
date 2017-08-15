583c83f88a18ad70daf002b1896cd3a3311b249d
Nico Schlömer <nico.schloemer@gmail.com>
2017-08-15 16:11:12 +0200

    don't require pipdate

--- meshio/__init__.py
+++ meshio/__init__.py
@@ -2,8 +2,6 @@
 #
 from __future__ import print_function
 
-import pipdate
-
 from .__about__ import (
     __version__,
     __author__,
@@ -14,5 +12,10 @@ from .__about__ import (
 # pylint: disable=wildcard-import
 from .helpers import *
 
-if pipdate.needs_checking(__name__):
-    print(pipdate.check(__name__, __version__))
+try:
+    import pipdate
+except ImportError:
+    pass
+else:
+    if pipdate.needs_checking(__name__):
+        print(pipdate.check(__name__, __version__))
