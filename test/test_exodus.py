015eb4e7dc8c319c9dbd5611de88261c477214ce
Nico Schlömer <nico.schloemer@gmail.com>
2017-11-06 10:36:24 +0100

    update comment

--- test/test_exodus.py
+++ test/test_exodus.py
@@ -9,10 +9,13 @@ vtk = pytest.importorskip('vtk')
 
 @pytest.mark.parametrize('mesh', [
         helpers.tri_mesh,
+        # helpers.quad_mesh,
+        # helpers.tet_mesh,
         helpers.add_point_data(helpers.tri_mesh, 2),
         helpers.add_point_data(helpers.tri_mesh, 3),
         ])
 def test_io(mesh):
-    # TODO report exodus precision failure
+    # Only single precision; see
+    # <https://gitlab.kitware.com/vtk/vtk/issues/17161>.
     helpers.write_read('test.e', 'exodus', mesh, 1.0e-8)
     return
