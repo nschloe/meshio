54a96feec6ce7551ba27040ea53eaecb52e9b920
Nico Schlömer <nico.schloemer@gmail.com>
2017-11-08 10:29:44 +0100

    cleanup for exodus

--- meshio/exodus_io.py
+++ meshio/exodus_io.py
@@ -8,79 +8,86 @@ I/O for Exodus etc.
 import numpy
 
 
+def merge_variables(variable_names):
+    # Exodus stores variables as "<name>_Z", "<name>_R" to form a
+    # two-dimensional value and "<name>X", "<name>Y", "<name>Z" to form a
+    # three-dimensional value. Separate those out first
+    n = len(variable_names)
+    is_accounted_for = n * [False]
+
+    variable_indices = {}
+
+    for k, var in enumerate(variable_names):
+        if is_accounted_for[k]:
+            continue
+
+        if var[-2:] == '_R':
+            k2 = [
+                i for i in range(k, n)
+                if variable_names[i] == var[:-2] + '_Z'
+                ]
+            assert len(k2) < 2
+            if k2:
+                variable_indices[var[:-2]] = [k, k2[0]]
+                is_accounted_for[k2[0]] = True
+            else:
+                variable_indices[var] = [k]
+            is_accounted_for[k] = True
+        elif var[-1] == 'X':
+            ky = [
+                i for i in range(k, n)
+                if variable_names[i] == var[:-1] + 'Y'
+                ]
+            assert len(ky) < 2
+            kz = [
+                i for i in range(k, n)
+                if variable_names[i] == var[:-1] + 'Z'
+                ]
+            assert len(kz) < 2
+            if ky and kz:
+                variable_indices[var[:-1]] = [k, ky[0], kz[0]]
+                is_accounted_for[ky[0]] = True
+                is_accounted_for[kz[0]] = True
+            else:
+                variable_indices[var] = [k]
+            is_accounted_for[k] = True
+        else:
+            variable_indices[var] = [k]
+            is_accounted_for[k] = True
+
+    assert all(is_accounted_for)
+
+    return variable_indices
+
+
 def read(filename):
     import netCDF4
 
     nc = netCDF4.Dataset(filename)
 
-    points = numpy.array([
-        nc.variables['coordx'][:],
-        nc.variables['coordy'][:],
-        nc.variables['coordz'][:],
-        ]).T
+    if 'coordx' in nc.variables:
+        points = numpy.array([
+            nc.variables['coordx'][:],
+            nc.variables['coordy'][:],
+            nc.variables['coordz'][:],
+            ]).T
+    else:
+        assert 'coord' in nc.variables
+        points = nc.variables['coord'][:].T
+        if points.shape[1] == 2:
+            points = numpy.column_stack([
+                points,
+                numpy.zeros(len(points))
+                ])
 
-    elem_types = {
-        'quad': 'quad',
-        'HEX': 'hexahedron',
-        'TETRA': 'tetra',
-        'TRIANGLE': 'triangle',
-        }
-
+    # handle point data
     point_data = {}
     if 'name_nod_var' in nc.variables:
         variable_names = [
             b''.join(c).decode('UTF-8')
             for c in nc.variables['name_nod_var'][:]
             ]
-
-        n = len(variable_names)
-        is_accounted_for = n * [False]
-
-        # Exodus stores variables as "<name>_Z", "<name>_R" to form a
-        # two-dimensional value and "<name>X", "<name>Y", "<name>Z" to form a
-        # three-dimensional value. Separate those out first
-        variable_indices = {}
-
-        for k, var in enumerate(variable_names):
-            if is_accounted_for[k]:
-                continue
-
-            if var[-2:] == '_R':
-                k2 = [
-                    i for i in range(k, n)
-                    if variable_names[i] == var[:-2] + '_Z'
-                    ]
-                assert len(k2) < 2
-                if k2:
-                    variable_indices[var[:-2]] = [k, k2[0]]
-                    is_accounted_for[k2[0]] = True
-                else:
-                    variable_indices[var] = [k]
-                is_accounted_for[k] = True
-            elif var[-1] == 'X':
-                ky = [
-                    i for i in range(k, n)
-                    if variable_names[i] == var[:-1] + 'Y'
-                    ]
-                assert len(ky) < 2
-                kz = [
-                    i for i in range(k, n)
-                    if variable_names[i] == var[:-1] + 'Z'
-                    ]
-                assert len(kz) < 2
-                if ky and kz:
-                    variable_indices[var[:-1]] = [k, ky[0], kz[0]]
-                    is_accounted_for[ky[0]] = True
-                    is_accounted_for[kz[0]] = True
-                else:
-                    variable_indices[var] = [k]
-                is_accounted_for[k] = True
-            else:
-                variable_indices[var] = [k]
-                is_accounted_for[k] = True
-
-        assert all(is_accounted_for)
-
+        variable_indices = merge_variables(variable_names)
         for name, indices in variable_indices.items():
             if len(indices) == 1:
                 point_data[name] = \
@@ -90,6 +97,15 @@ def read(filename):
                     nc.variables['vals_nod_var{}'.format(index+1)][:]
                     for index in indices
                     ]).T
+
+    elem_types = {
+        'HEX': 'hexahedron',
+        'quad': 'quad',
+        'SHELL4': 'quad',
+        'TETRA': 'tetra',
+        'TRI3': 'triangle',
+        'TRIANGLE': 'triangle',
+        }
 
     cells = {}
     for key in nc.variables:
