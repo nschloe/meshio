afc622067789f705424283e437575ddc810a151a
Nico Schlömer <nico.schloemer@gmail.com>
2017-12-01 16:10:18 +0100

    be less strict in permas filename check

--- test/test_off.py
+++ test/test_off.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 #
-import helpers
-
+import meshio
 import pytest
+
+import helpers
 
 
 @pytest.mark.parametrize('mesh', [
-        helpers.tri_mesh
-        ])
+    helpers.tri_mesh
+    ])
 def test_io(mesh):
-    helpers.write_read('test.off', 'off', mesh, 1.0e-15)
+    helpers.write_read2(
+        meshio.off_io.write,
+        meshio.off_io.read,
+        mesh, 1.0e-15
+        )
     return
