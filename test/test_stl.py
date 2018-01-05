f6ec68c853e38fb40b1ab74c3f61cf03f4393f5a
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-05 16:42:06 +0100

    stl: binary i/o

--- test/test_stl.py
+++ test/test_stl.py
@@ -10,10 +10,14 @@ import helpers
 @pytest.mark.parametrize('mesh', [
     helpers.tri_mesh,
     ])
-@pytest.mark.parametrize('write_binary', [False, True])
-def test_stl(mesh, write_binary):
+@pytest.mark.parametrize('write_binary, tol', [
+    (False, 1.0e-15),
+    # binary STL only operates in single precision
+    (True, 1.0e-8),
+    ])
+def test_stl(mesh, write_binary, tol):
     def writer(*args, **kwargs):
         return meshio.stl_io.write(*args, write_binary=write_binary, **kwargs)
 
-    helpers.write_read(writer, meshio.stl_io.read, mesh, 1.0e-15)
+    helpers.write_read(writer, meshio.stl_io.read, mesh, tol)
     return
