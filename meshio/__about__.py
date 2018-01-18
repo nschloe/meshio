3bf339c8a2da765781f936e0e7af4e76810305b6
Nico Schlömer <nico.schloemer@gmail.com>
2018-01-18 09:03:46 +0100

    version bump

--- meshio/__about__.py
+++ meshio/__about__.py
@@ -2,7 +2,9 @@
 #
 __version__ = '1.11.3'
 __author__ = u'Nico Schlömer'
-__author_email__ = 'nico.schloemer@gmail.com'
+__email__ = 'nico.schloemer@gmail.com'
+__copyright__ = \
+    u'Copyright (c) 2015-2018, {} <{}>'.format(__author__, __email__)
 __website__ = 'https://github.com/nschloe/meshio'
 __license__ = 'License :: OSI Approved :: MIT License'
 __status__ = 'Development Status :: 5 - Production/Stable'
