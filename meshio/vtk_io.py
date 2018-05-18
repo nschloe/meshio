4fe2b7e6281a8479e81950226b9c804c4d362e7f
Nico Schlömer <nico.schloemer@gmail.com>
2018-05-18 20:48:59 +0200

    pylint fix

--- meshio/vtk_io.py
+++ meshio/vtk_io.py
@@ -58,32 +58,32 @@ vtk_to_meshio_type = {
 meshio_to_vtk_type = {v: k for k, v in vtk_to_meshio_type.items()}
 
 vtk_type_to_numnodes = {
-        0: 0,  # empty
-        1: 1,  # vertex
-        3: 2,  # line
-        5: 3,  # triangle
-        9: 4,  # quad
-        10: 4,  # tetra
-        12: 8,  # hexahedron
-        13: 6,  # wedge
-        14: 5,  # pyramid
-        15: 10,  # penta_prism
-        16: 12,  # hexa_prism
-        21: 3,  # line3
-        22: 6,  # triangle6
-        23: 8,  # quad8
-        24: 10,  # tetra10
-        25: 20,  # hexahedron20
-        26: 15,  # wedge15
-        27: 13,  # pyramid13
-        28: 9,  # quad9
-        29: 27,  # hexahedron27
-        30: 6,  # quad6
-        31: 12,  # wedge12
-        32: 18,  # wedge18
-        33: 24,  # hexahedron24
-        34: 7,  # triangle7
-        35: 4,  # line4
+    0: 0,  # empty
+    1: 1,  # vertex
+    3: 2,  # line
+    5: 3,  # triangle
+    9: 4,  # quad
+    10: 4,  # tetra
+    12: 8,  # hexahedron
+    13: 6,  # wedge
+    14: 5,  # pyramid
+    15: 10,  # penta_prism
+    16: 12,  # hexa_prism
+    21: 3,  # line3
+    22: 6,  # triangle6
+    23: 8,  # quad8
+    24: 10,  # tetra10
+    25: 20,  # hexahedron20
+    26: 15,  # wedge15
+    27: 13,  # pyramid13
+    28: 9,  # quad9
+    29: 27,  # hexahedron27
+    30: 6,  # quad6
+    31: 12,  # wedge12
+    32: 18,  # wedge18
+    33: 24,  # hexahedron24
+    34: 7,  # triangle7
+    35: 4,  # line4
     }
 
 
