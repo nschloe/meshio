177ab969a7765e216c0576f7268768aae13cbdf1
Nico Schlömer <nico.schloemer@gmail.com>
2018-06-10 12:44:14 +0200

    version bump

--- meshio/__about__.py
+++ meshio/__about__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-__version__ = "1.11.19"
+__version__ = "1.11.20"
 __author__ = u"Nico Schlömer"
 __author_email__ = "nico.schloemer@gmail.com"
 __copyright__ = u"Copyright (c) 2015-2018, {} <{}>".format(__author__, __author_email__)
