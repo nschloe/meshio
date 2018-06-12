b28074e77e30227f4314896356470a494ec13ba6
hillyuan <hillyuan.jp@gmail.com>
2018-06-12 23:11:42 +0900

    modify node sets' name

--- test/helpers.py
+++ test/helpers.py
@@ -218,7 +218,7 @@ def add_field_data(mesh, value, dtype):
 
 def add_node_sets(mesh):
     mesh2 = copy.deepcopy(mesh)
-    mesh2.node_sets = {"nset": numpy.array([1, 2])}
+    mesh2.node_sets = {"fixed": numpy.array([1, 2])}
     return mesh2
 
 
