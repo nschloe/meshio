d1af90f77b327a655bad21d9568cc90535297950
G. D. McBain <gdmcbain@protonmail.com>
2018-08-31 16:54:39 +1000

    read "Hexaedra" (Frey) or "Hexahedra" (Dobrzynski) (but write latter)

--- meshio/medit_io.py
+++ meshio/medit_io.py
@@ -67,7 +67,8 @@ def read_buffer(file):
         "Triangles": ("triangle", 3),
         "Quadrilaterals": ("quad", 4),
         "Tetrahedra": ("tetra", 4),
-        "Hexahedra": ("hexahedron", 8),
+        "Hexahedra": ("hexahedron", 8),  # Frey
+        "Hexaedra": ("hexahedron", 8),   # Dobrzynski
     }
 
     reader = _ItemReader(file)
