f875767d1c1fa167216cbb1857240ab9e1b6bf1c
Nico Schlömer <nico.schloemer@gmail.com>
2019-03-21 12:37:17 +0100

    remove pipdate
    Fixes #305.

--- meshio/__init__.py
+++ meshio/__init__.py
@@ -23,11 +23,3 @@ __all__ = [
     "__author_email__",
     "__website__",
 ]
-
-try:
-    import pipdate
-except ImportError:
-    pass
-else:
-    if pipdate.needs_checking(__name__):
-        print(pipdate.check(__name__, __version__), end="")
