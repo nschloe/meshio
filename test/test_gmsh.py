a2512f93cad34f933777d65f79b1299c7c2bb815
Nico Schlömer <nico.schloemer@gmail.com>
2019-04-04 12:19:00 +0200

    test gmsh 4.0 I/O

--- test/test_gmsh.py
+++ test/test_gmsh.py
@@ -73,6 +73,37 @@ def test_gmsh2(mesh, write_binary):
         helpers.add_field_data(helpers.tri_mesh, [1, 2], int),
         helpers.add_field_data(helpers.tet_mesh, [1, 3], int),
         helpers.add_field_data(helpers.hex_mesh, [1, 3], int),
+    ],
+)
+@pytest.mark.parametrize("write_binary", [False, True])
+def test_gmsh40(mesh, write_binary):
+    writer = partial(meshio.msh_io.write, fmt_version="4.0", write_binary=write_binary)
+
+    helpers.write_read(writer, meshio.msh_io.read, mesh, 1.0e-15)
+    return
+
+
+@pytest.mark.parametrize(
+    "mesh",
+    [
+        helpers.tri_mesh,
+        helpers.triangle6_mesh,
+        helpers.quad_mesh,
+        helpers.quad8_mesh,
+        helpers.tri_quad_mesh,
+        helpers.tet_mesh,
+        helpers.tet10_mesh,
+        helpers.hex_mesh,
+        helpers.hex20_mesh,
+        helpers.add_point_data(helpers.tri_mesh, 1),
+        helpers.add_point_data(helpers.tri_mesh, 3),
+        helpers.add_point_data(helpers.tri_mesh, 9),
+        helpers.add_cell_data(helpers.tri_mesh, 1),
+        helpers.add_cell_data(helpers.tri_mesh, 3),
+        helpers.add_cell_data(helpers.tri_mesh, 9),
+        helpers.add_field_data(helpers.tri_mesh, [1, 2], int),
+        helpers.add_field_data(helpers.tet_mesh, [1, 3], int),
+        helpers.add_field_data(helpers.hex_mesh, [1, 3], int),
         gmsh_periodic(),
     ],
 )
