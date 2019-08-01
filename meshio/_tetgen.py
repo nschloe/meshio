4038789c6e102b2ccc8a37a256d2665db5e9eb05
Nico Schlömer <nico.schloemer@gmail.com>
2019-08-01 18:52:47 +0200

    add tetgen writer

--- meshio/_tetgen.py
+++ meshio/_tetgen.py
@@ -3,10 +3,12 @@ I/O for the TetGen file format, c.f.
 <https://wias-berlin.de/software/tetgen/fformats.node.html>
 """
 import os
+import logging
 from itertools import islice
 
 import numpy
 
+from .__about__ import __version__
 from ._mesh import Mesh
 
 
@@ -22,15 +24,21 @@ def read(filename):
 
     # read nodes
     with open(node_filename) as f:
-        line = next(islice(f, 1))
+        line = next(islice(f, 1)).strip()
+        while line[0] == "#":
+            line = next(islice(f, 1)).strip()
+
         num_points, dim, num_attrs, num_bmarkers = [
-            int(item) for item in line.strip().split(" ") if item != ""
+            int(item) for item in line.split(" ") if item != ""
         ]
         assert dim == 3
         flt = numpy.vectorize(float)
         points = numpy.empty((num_points, 3), dtype=float)
         for k in range(num_points):
             line = next(islice(f, 1)).strip()
+            if line[0] == "#":
+                continue
+
             out = [item for item in line.split(" ") if item != ""]
             assert len(out) == 4 + num_attrs + num_bmarkers
             points[k] = flt(out[1:4])
@@ -42,7 +50,10 @@ def read(filename):
 
     # read elements
     with open(ele_filename) as f:
-        line = next(islice(f, 1))
+        line = next(islice(f, 1)).strip()
+        while line[0] == "#":
+            line = next(islice(f, 1)).strip()
+
         num_tets, num_points_per_tet, num_attrs = [
             int(item) for item in line.strip().split(" ") if item != ""
         ]
@@ -50,6 +61,9 @@ def read(filename):
         cells = numpy.empty((num_tets, 4), dtype=int)
         for k in range(num_tets):
             line = next(islice(f, 1)).strip()
+            if line[0] == "#":
+                continue
+
             out = [item for item in line.split(" ") if item != ""]
             assert len(out) == 5 + num_attrs
             cells[k] = [int(item) for item in out[1:5]]
@@ -58,3 +72,41 @@ def read(filename):
         cells -= node_index_base
 
     return Mesh(points, {"tetra": cells})
+
+
+def write(filename, mesh):
+
+    base, ext = os.path.splitext(filename)
+    if ext == ".node":
+        node_filename = filename
+        ele_filename = base + ".ele"
+    else:
+        assert ext == ".ele"
+        node_filename = base + ".node"
+        ele_filename = filename
+
+    assert mesh.points.shape[1] == 3
+
+    # write nodes
+    with open(node_filename, "w") as fh:
+        fh.write("# This file was created by meshio v{}\n".format(__version__))
+        fh.write("{} {} {} {}\n".format(mesh.points.shape[0], 3, 0, 0))
+        for k, pt in enumerate(mesh.points):
+            fh.write("{} {:.15e} {:.15e} {:.15e}\n".format(k, pt[0], pt[1], pt[2]))
+
+    assert "tetra" in mesh.cells, "TegGen only supports tetrahedra"
+    if len(mesh.cells) > 1:
+        logging.warning(
+            "TetGen only supports tetrahedra, but mesh has {}. Skipping those.".format(
+                ", ".join([key for key in mesh.keys() if key != "tetra"])
+            )
+        )
+
+    # write cells
+    with open(ele_filename, "w") as fh:
+        fh.write("# This file was created by meshio v{}\n".format(__version__))
+        fh.write("{} {} {}\n".format(mesh.cells["tetra"].shape[0], 4, 0))
+        for k, tet in enumerate(mesh.cells["tetra"]):
+            fh.write("{} {} {} {} {}\n".format(k, tet[0], tet[1], tet[2], tet[3]))
+
+    return
