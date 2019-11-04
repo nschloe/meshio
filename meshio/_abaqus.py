04ee40f93752dfa1f13254cb0efacdff1eade6a8
Darcy Beurle <darcy@beurle.id.au>
2019-11-04 15:38:52 +0100

    Combine failure statements

--- meshio/_abaqus.py
+++ meshio/_abaqus.py
@@ -228,9 +228,7 @@ def read_set(f, params_map):
     set_ids = []
     while True:
         line = f.readline()
-        if not line:
-            break
-        if line.startswith("*"):
+        if not line or line.startswith("*"):
             break
         set_ids += [int(k) for k in line.strip().strip(",").split(",")]
 
