edfd72e480171045752b4eb2ee9bf96a860ae900
Nico Schlömer <nico.schloemer@gmail.com>
2019-12-06 11:32:52 +0100

    {from,to}_file -> read/write

--- meshio/_xdmf/main.py
+++ meshio/_xdmf/main.py
@@ -339,7 +339,6 @@ class XdmfWriter:
         ET.register_namespace("xi", "https://www.w3.org/2001/XInclude/")
 
         write_xml(filename, xdmf_file, pretty_xml)
-        return
 
     def numpy_to_xml_string(self, data):
         if self.data_format == "XML":
