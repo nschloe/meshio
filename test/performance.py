784a1abee13a03bda4f762fcd47f07a8e79e11bb
Nico Schlömer <nico.schloemer@gmail.com>
2019-12-10 11:41:56 +0100

    better performance plots

--- test/performance.py
+++ test/performance.py
@@ -15,50 +15,99 @@ def generate_mesh():
 def read_write(plot=False):
     mesh = generate_mesh()
 
-    formats = [
-        "abaqus",
-        "ansys-ascii",
-        "ansys-binary",
-        "exodus",
-        "dolfin-xml",
-        "gmsh2-ascii",
-        "gmsh2-binary",
-        "gmsh4-ascii",
-        "gmsh4-binary",
-        "mdpa",
-        "med",
-        "medit",
-        # "moab",
-        "nastran",
-        "off",
-        "permas",
-        "ply-ascii",
-        "ply-binary",
-        "stl-ascii",
-        "stl-binary",
-        "vtk-ascii",
-        "vtk-binary",
-        "vtu-ascii",
-        "vtu-binary",
-        "xdmf",
-    ]
+    formats = {
+        "Abaqus": (meshio.abaqus.write, meshio.abaqus.read),
+        "ANSYS (ASCII)": (
+            lambda f, m: meshio.ansys.write(f, m, binary=False),
+            meshio.ansys.read,
+        ),
+        "ANSYS (binary)": (
+            lambda f, m: meshio.ansys.write(f, m, binary=True),
+            meshio.ansys.read,
+        ),
+        "Exodus": (meshio.exodus.write, meshio.exodus.read),
+        "Dolfin XML": (meshio.dolfin.write, meshio.dolfin.read),
+        "Gmsh 4.1 (ASCII)": (
+            lambda f, m: meshio.gmsh.write(f, m, binary=False),
+            meshio.gmsh.read,
+        ),
+        "Gmsh 4.1 (binary)": (
+            lambda f, m: meshio.gmsh.write(f, m, binary=True),
+            meshio.gmsh.read,
+        ),
+        # "MDPA": (meshio.mdpa.write, meshio.mdpa.read),
+        "MED": (meshio.med.write, meshio.med.read),
+        "Medit": (meshio.medit.write, meshio.medit.read),
+        "MOAB": (meshio.h5m.write, meshio.h5m.read),
+        # "Nastran": (meshio.nastran.write, meshio.nastran.read),
+        # "OFF": (meshio.off.write, meshio.off.read),
+        "Permas": (meshio.permas.write, meshio.permas.read),
+        # "PLY (ASCII)": (
+        #     lambda f, m: meshio.ply.write(f, m, binary=False),
+        #     meshio.ply.read,
+        # ),
+        # "PLY (binary)": (
+        #     lambda f, m: meshio.ply.write(f, m, binary=True),
+        #     meshio.ply.read,
+        # ),
+        "STL (ASCII)": (
+            lambda f, m: meshio.stl.write(f, m, binary=False),
+            meshio.stl.read,
+        ),
+        "STL (binary)": (
+            lambda f, m: meshio.stl.write(f, m, binary=True),
+            meshio.stl.read,
+        ),
+        # "VTK (ASCII)": (
+        #     lambda f, m: meshio.vtk.write(f, m, binary=False),
+        #     meshio.vtk.read,
+        # ),
+        "VTK (binary)": (
+            lambda f, m: meshio.vtk.write(f, m, binary=True),
+            meshio.vtk.read,
+        ),
+        "VTU (ASCII)": (
+            lambda f, m: meshio.vtu.write(f, m, binary=False),
+            meshio.vtu.read,
+        ),
+        "VTU (binary)": (
+            lambda f, m: meshio.vtu.write(f, m, binary=True),
+            meshio.vtu.read,
+        ),
+        "XDMF (XML)": (
+            lambda f, m: meshio.xdmf.write(f, m, data_format="XML"),
+            meshio.xdmf.read,
+        ),
+        "XDMF (binary)": (
+            lambda f, m: meshio.xdmf.write(f, m, data_format="Binary"),
+            meshio.xdmf.read,
+        ),
+        "XDMF (HDF, uncompressed)": (
+            lambda f, m: meshio.xdmf.write(f, m, data_format="HDF", compression=None),
+            meshio.xdmf.read,
+        ),
+        "XDMF (HDF, GZIP)": (
+            lambda f, m: meshio.xdmf.write(f, m, data_format="HDF", compression="gzip"),
+            meshio.xdmf.read,
+        ),
+    }
 
     elapsed_write = []
     elapsed_read = []
 
     print()
-    print("format        write (s)    read(s)")
+    print("format             write (s)    read(s)")
     print()
-    for fmt in formats:
+    for name, (writer, reader) in formats.items():
         filename = tempfile.NamedTemporaryFile().name
         t = time.time()
-        meshio.write(filename, mesh, file_format=fmt)
+        writer(filename, mesh)
         elapsed_write.append(time.time() - t)
 
         t = time.time()
-        meshio.read(filename, file_format=fmt)
+        reader(filename)
         elapsed_read.append(time.time() - t)
-        print("{0: <12}  {1:e} {2:e}".format(fmt, elapsed_write[-1], elapsed_read[-1]))
+        print("{:<17}  {:e} {:e}".format(name, elapsed_write[-1], elapsed_read[-1]))
 
     if plot:
         import numpy as np
@@ -66,27 +115,28 @@ def read_write(plot=False):
 
         elapsed_write = np.asarray(elapsed_write)
         elapsed_read = np.asarray(elapsed_read)
-        formats = np.asarray(formats)
+        formats = np.asarray(list(formats.keys()))
 
         fig, ax = plt.subplots(1, 2, figsize=(12, 4))
 
-        idx = np.argsort(elapsed_write)
-        ax[0].bar(range(len(formats)), elapsed_write[idx], align="center")
-        ax[0].set_xticks(range(len(formats)))
-        ax[0].set_xticklabels(formats[idx], rotation=90)
-        ax[0].set_ylabel("time (s)")
+        idx = np.argsort(elapsed_write)[::-1]
+        ax[0].barh(range(len(formats)), elapsed_write[idx], align="center")
+        ax[0].set_yticks(range(len(formats)))
+        ax[0].set_yticklabels(formats[idx])
+        ax[0].set_xlabel("time (s)")
         ax[0].set_title("write")
         ax[0].grid()
 
-        idx = np.argsort(elapsed_read)
-        ax[1].bar(range(len(formats)), elapsed_read[idx], align="center")
-        ax[1].set_xticks(range(len(formats)))
-        ax[1].set_xticklabels(formats[idx], rotation=90)
-        ax[1].set_ylabel("time (s)")
+        idx = np.argsort(elapsed_read)[::-1]
+        ax[1].barh(range(len(formats)), elapsed_read[idx], align="center")
+        ax[1].set_yticks(range(len(formats)))
+        ax[1].set_yticklabels(formats[idx])
+        ax[1].set_xlabel("time (s)")
         ax[1].set_title("read")
         ax[1].grid()
 
         fig.tight_layout()
+        # plt.show()
         fig.savefig("performance.svg", transparent=True, bbox_inches="tight")
 
 
