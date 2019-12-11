297187f138491069535077d7ea1a778e5ad5f492
Nico Schlömer <nico.schloemer@gmail.com>
2019-12-11 15:22:52 +0100

    skipif fix

--- test/test_helpers.py
+++ test/test_helpers.py
@@ -33,7 +33,7 @@ def test_write_str(mesh, tmpdir):
     assert Path(tmp_path).is_file()
 
 
-@pytest.mark.skipif(sys.version_info < (3, 6))
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="Fails with 3.5")
 def test_write_pathlike(mesh, tmpdir):
     tmp_path = Path(tmpdir.join("tmp.obj"))
     meshio.write(tmp_path, mesh)
