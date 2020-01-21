6e645e089ca1eda327a66a2eb1bca88180aad1b8
Nico Schlömer <nico.schloemer@gmail.com>
2020-01-21 18:45:00 +0100

    add cells_dict property to mesh

--- meshio/_mesh.py
+++ meshio/_mesh.py
@@ -114,6 +114,13 @@ class Mesh:
 
         write(path_or_buf, self, file_format, **kwargs)
 
+    @property
+    def cells_dict(self):
+        assert len(self.cells) == len(
+            numpy.unique([c.type for c in self.cells])
+        ), "More than one block of the same type. Cannot create dictionary."
+        return {cell_type: data for cell_type, data in self.cells}
+
     @classmethod
     def read(cls, path_or_buf, file_format=None):
         # avoid circular import
--- test/test_mesh.py
+++ test/test_mesh.py
@@ -1,10 +1,17 @@
 import copy
 
+import numpy
+
 import helpers
 
 
-def test():
+def test_print_prune():
     mesh = copy.deepcopy(helpers.tri_mesh)
     print(mesh)
     mesh.prune()
-    return
+
+
+def test_cells_dict():
+    mesh = copy.deepcopy(helpers.tri_mesh)
+    assert len(mesh.cells_dict) == 1
+    assert numpy.array_equal(mesh.cells_dict["triangle"], [[0, 1, 2], [0, 2, 3]])
