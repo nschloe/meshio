d31fb10989586f48d837e458d5814ca1384ab6f4
Nico Schlömer <nico.schloemer@gmail.com>
2020-02-11 13:46:01 +0100

    write xml_declaration

--- meshio/_cxml/etree.py
+++ meshio/_cxml/etree.py
@@ -53,6 +53,8 @@ class ElementTree:
     def __init__(self, root):
         self.root = root
 
-    def write(self, filename):
+    def write(self, filename, xml_declaration=True):
         with open(filename, "w") as f:
+            if xml_declaration:
+                f.write('<?xml version="1.0"?>\n')
             self.root.write(f)
