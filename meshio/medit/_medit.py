77add75a3183dc6a239372b9900ed29f35ed1f94
Christos Tsolakis <ctsolakis@cs.odu.edu>
2020-04-19 13:55:44 -0400

    initial working version of binary-writer

--- meshio/medit/_medit.py
+++ meshio/medit/_medit.py
@@ -91,7 +91,7 @@ def read_binary_buffer(f):
     if code != 1 and code != 16777216:
         raise ReadError("Invalid code")
 
-    print("% : % ", get_linenumber(), code)
+    print("%d : %d " % (get_linenumber(), code))
 
     if code == 16777216:
         # swap endianess
@@ -99,7 +99,8 @@ def read_binary_buffer(f):
         ftype += "S"
         postype += "S"
 
-    version = numpy.fromfile(f, count=1, dtype="i4")
+    version = numpy.fromfile(f, count=1, dtype="i4").item()
+    print("%d : %d " % (get_linenumber(), version))
 
     if version < 1 or version > 4:
         raise ReadError("Invalid version")
@@ -124,7 +125,7 @@ def read_binary_buffer(f):
     field = numpy.fromfile(f, count=1, dtype=postype).item()
 
     if field != 3:  # =  GmfDimension
-        raise ReadError("Invalid dimension code")
+        raise ReadError("Invalid dimension code : " + str(field) + " it should be 3")
 
     pos = numpy.fromfile(f, count=1, dtype=postype)
     print("%d : %d " % (get_linenumber(), pos))
@@ -157,6 +158,7 @@ def read_binary_buffer(f):
             continue
 
         pos = numpy.fromfile(f, count=1, dtype=postype)
+        print("%d : %d " % (get_linenumber(), pos))
 
         nitems = 1
         if field_code[1] == "i":
@@ -179,7 +181,7 @@ def read_binary_buffer(f):
             # for the cell connectivity
             out_view = out.view(itype).reshape(nitems, ncols + 1)
             cells.append((meshio_type, out_view[:, :ncols] - 1))
-            cell_data["medit:ref"].append(out_view[:,-1])
+            cell_data["medit:ref"].append(out_view[:, -1])
 
     return Mesh(points, cells, point_data=point_data, cell_data=cell_data)
 
@@ -260,6 +262,14 @@ def read_buffer(f):
 
 
 def write(filename, mesh, float_fmt=".15e"):
+    print(filename)
+    if filename[-1] == "b":
+        write_binary_file(filename, mesh)
+    else:
+        write_ascii_file(filename, mesh, float_fmt)
+
+
+def write_ascii_file(filename, mesh, float_fmt=".15e"):
     with open_file(filename, "wb") as fh:
         version = {numpy.dtype(c_float): 1, numpy.dtype(c_double): 2}[mesh.points.dtype]
         # N. B.: PEP 461 Adding % formatting to bytes and bytearray
@@ -328,6 +338,198 @@ def write(filename, mesh, float_fmt=".15e"):
                 fh.write(fmt.format(*d, label).encode("utf-8"))
 
         fh.write(b"\nEnd\n")
+
+
+def write_binary_file(f, mesh):
+    with open_file(f, "wb") as fh:
+
+        postype = "i4"
+        itype = "i4"
+        ftype = "f8"
+        int_size = 4
+
+        code = 1
+
+        version = 2
+
+        field_code = 3  # GmfDimension
+
+        pos = 20  # header size in bytes
+
+        num_verts, dim = mesh.points.shape
+
+        tmp_array = numpy.array([code, version, field_code, pos, dim], dtype="i4")
+
+        tmp_array.astype("i4").tofile(fh)
+
+        # points
+        field = 4  # GmfVertices
+        field_code = medit_codes[field]
+
+        float_size = 4
+        # TODO is this safe ?
+        if mesh.points.dtype == "f8":
+            float_size = 8
+
+        pos += num_verts * dim * float_size + num_verts * int_size + 3 * 4
+
+        tmp_array = numpy.array([field, pos, num_verts], dtype="i4")
+        tmp_array.astype("i4").tofile(fh)
+
+        field_template = field_code[2]
+        dtype = numpy.dtype(_produce_dtype(field_template, dim, itype, ftype))
+        print(dtype)
+
+        labels_key, other = _pick_first_int_data(mesh.point_data)
+        if labels_key and other:
+            logging.warning(
+                "Medit can only write one point data array. "
+                "Picking {}, skipping {}.".format(labels_key, ", ".join(other))
+            )
+        labels = (
+            mesh.point_data[labels_key] if labels_key else numpy.ones(n, dtype=itype)
+        )
+
+        tmp_array = numpy.empty(num_verts, dtype=dtype)
+        tmp_array["f0"] = mesh.points
+        tmp_array["f1"] = labels
+        tmp_array.tofile(fh)
+
+        labels_key, other = _pick_first_int_data(mesh.cell_data)
+        if labels_key and other:
+            logging.warning(
+                "Medit can only write one cell data array. "
+                "Picking {}, skipping {}.".format(labels_key, ", ".join(other))
+            )
+
+        # first component is medit keyword id see _medit_internal.py
+        medit_from_meshio = {
+            "line": 5,
+            "triangle": 6,
+            "quad": 7,
+            "tetra": 8,
+            "wedge": 9,
+            "hexahedron": 10,
+        }
+        for k, (cell_type, data) in enumerate(mesh.cells):
+            try:
+                medit_key = medit_from_meshio[cell_type]
+            except KeyError:
+                msg = ("MEDIT's mesh format doesn't know {} cells. Skipping.").format(
+                    cell_type
+                )
+                logging.warning(msg)
+                continue
+
+            num_cells, num_verts = data.shape
+
+            pos += num_cells * (num_verts + 1) * int_size + 3 * 4
+
+            tmp_array = numpy.array([medit_key, pos, num_cells], dtype="i4")
+            tmp_array.tofile(fh)
+
+            # pick out cell data
+            labels = (
+                mesh.cell_data[labels_key][k]
+                if labels_key
+                else numpy.ones(len(data), dtype=data.dtype)
+            )
+            field_template = medit_codes[medit_key][2]
+            dtype = numpy.dtype(_produce_dtype(field_template, dim, itype, ftype))
+            print(dtype)
+
+            tmp_array = numpy.empty(num_cells, dtype=dtype)
+            i = 0
+            print(data.shape)
+            for col_type in dtype.names[:-1]:
+                print(col_type)
+                tmp_array[col_type] = data[:, i] + 1
+                print(len(data[:, i]))
+                i += 1
+
+            tmp_array[dtype.names[-1]] = labels
+            print(tmp_array.shape)
+            print(tmp_array[0])
+            tmp_array.tofile(fh)
+
+        pos = 0  # 2*int_size
+        tmp_array = numpy.array([54, pos], dtype="i4")
+        tmp_array.tofile(fh)
+
+
+def read_buffer(f):
+    dim = 0
+    cells = []
+    point_data = {}
+    cell_data = {"medit:ref": []}
+
+    meshio_from_medit = {
+        "Edges": ("line", 2),
+        "Triangles": ("triangle", 3),
+        "Quadrilaterals": ("quad", 4),
+        "Tetrahedra": ("tetra", 4),
+        "Hexahedra": ("hexahedron", 8),  # Frey
+        "Hexaedra": ("hexahedron", 8),  # Dobrzynski
+    }
+
+    while True:
+        line = f.readline()
+        if not line:
+            # EOF
+            break
+
+        line = line.strip()
+        if len(line) == 0 or line[0] == "#":
+            continue
+
+        items = line.split()
+
+        if not items[0].isalpha():
+            raise ReadError()
+
+        if items[0] == "MeshVersionFormatted":
+            version = items[1]
+            dtype = {"1": c_float, "2": c_double}[version]
+        elif items[0] == "Dimension":
+            dim = int(items[1])
+        elif items[0] == "Vertices":
+            if dim <= 0:
+                raise ReadError()
+            num_verts = int(f.readline())
+            out = numpy.fromfile(
+                f, count=num_verts * (dim + 1), dtype=dtype, sep=" "
+            ).reshape(num_verts, dim + 1)
+            points = out[:, :dim]
+            point_data["medit:ref"] = out[:, dim].astype(int)
+        elif items[0] in meshio_from_medit:
+            meshio_type, points_per_cell = meshio_from_medit[items[0]]
+            # The first value is the number of elements
+            num_cells = int(f.readline())
+
+            out = numpy.fromfile(
+                f, count=num_cells * (points_per_cell + 1), dtype=int, sep=" "
+            ).reshape(num_cells, points_per_cell + 1)
+
+            # adapt for 0-base
+            cells.append((meshio_type, out[:, :points_per_cell] - 1))
+            cell_data["medit:ref"].append(out[:, -1])
+        elif items[0] == "Normals":
+            # those are just discarded
+            num_normals = int(f.readline())
+            numpy.fromfile(f, count=num_normals * dim, dtype=dtype, sep=" ").reshape(
+                num_normals, dim
+            )
+        elif items[0] == "NormalAtVertices":
+            # those are just discarded
+            num_normal_at_vertices = int(f.readline())
+            numpy.fromfile(
+                f, count=num_normal_at_vertices * 2, dtype=int, sep=" "
+            ).reshape(num_normal_at_vertices, 2)
+        else:
+            if items[0] != "End":
+                raise ReadError("Unknown keyword '{}'.".format(items[0]))
+
+    return Mesh(points, cells, point_data=point_data, cell_data=cell_data)
 
 
 register("medit", [".mesh", ".meshb"], read, {"medit": write})
