3833567081bfd57d81a329ddbd9d18e5ce75815d
chrishavlin <chris.havlin@gmail.com>
2020-07-02 15:41:56 -0500

    vtu_offset_fix: down to one line

--- meshio/vtu/_vtu.py
+++ meshio/vtu/_vtu.py
@@ -363,8 +363,7 @@ class VtuReader:
         if len(cell_data_raw) != len(cells):
             raise ReadError()
 
-        point_offsets = [0]
-        point_offsets.extend(numpy.cumsum([pts.shape[0] for pts in points])[:-1])
+	point_offsets = numpy.cumsum([0]+[pts.shape[0] for pts in points][:-1])
 
         # Now merge across pieces
         if not points:
