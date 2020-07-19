7e875efe4262c7ad7b3b83039c3421ed4d87a1cf
Christos Tsolakis <ctsolakis@cs.odu.edu>
2020-07-18 23:25:04 -0400

    add tests

---
+++ test/meshes/su2/README.md
@@ -0,0 +1,2 @@
+* `square.su2` : from https://su2code.github.io/docs_v7/Mesh-File/
+* `mixgrid.su2` : generated using http://ossanworld.com/cfdbooks/cfdcodes/mixgrid_cube_v5.f90
---
+++ test/test_su2.py
@@ -0,0 +1,45 @@
+import os
+
+import helpers
+import numpy
+import pytest
+
+import meshio
+
+test_set = [
+    helpers.tri_mesh_2d,
+    helpers.tet_mesh,
+    helpers.hex_mesh,
+]
+
+
+@pytest.mark.parametrize("mesh", test_set)
+def test(mesh):
+    def writer(*args, **kwargs):
+        return meshio.su2.write(*args, **kwargs)
+
+    helpers.write_read(writer, meshio.su2.read, mesh, 1.0e-15)
+
+
+@pytest.mark.parametrize(
+    "filename, ref_num_cells, ref_num_points,ref_num_unique_tags,sum_tags",
+    [("square.su2", 16, 9, 4, 20), ("mixgrid.su2", 30, 16, 6, 62)],
+)
+def test_structured(
+    filename, ref_num_cells, ref_num_points, ref_num_unique_tags, sum_tags
+):
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(this_dir, "meshes", "su2", filename)
+
+    mesh = meshio.read(filename)
+
+    assert sum(len(block.data) for block in mesh.cells) == ref_num_cells
+    assert len(mesh.points) == ref_num_points
+
+    all_tags = numpy.concatenate([tags for tags in mesh.cell_data["su2:tag"]])
+
+    assert sum(all_tags) == sum_tags
+
+    all_unique_tags = numpy.unique(all_tags)
+
+    assert len(all_unique_tags) == ref_num_unique_tags + 1
