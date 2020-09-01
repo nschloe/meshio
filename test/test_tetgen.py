0be4808b59fe537c9d534bb2948bde997effab6c
Vladimir Lukes <vlukes@kme.zcu.cz>
2020-09-01 14:54:04 +0200

    test reading region/boundary markers

--- test/test_tetgen.py
+++ test/test_tetgen.py
@@ -1,3 +1,5 @@
+import pathlib
+
 import helpers
 import pytest
 
@@ -11,3 +13,15 @@ def test(mesh):
     helpers.write_read(
         meshio.tetgen.write, meshio.tetgen.read, mesh, 1.0e-15, extension=".node"
     )
+
+
+@pytest.mark.parametrize(
+    "filename, point_ref_sum, cell_ref_sum", [("mesh.ele", 12, 373)]
+)
+def test_point_cell_refs(filename, point_ref_sum, cell_ref_sum):
+    this_dir = pathlib.Path(__file__).resolve().parent
+    filename = this_dir / "meshes" / "tetgen" / filename
+
+    mesh = meshio.read(filename)
+    assert mesh.point_data["tetgen:ref"].sum() == point_ref_sum
+    assert mesh.cell_data["tetgen:ref"][0].sum() == cell_ref_sum
