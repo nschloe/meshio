7365a739660927f373cbf613e1d869265fb16e7d
Nico Schlömer <nico.schloemer@gmail.com>
2020-11-30 18:03:20 +0100

    remove some obsolete code

--- meshio/_files.py
+++ meshio/_files.py
@@ -1,11 +1,4 @@
-import sys
 from contextlib import contextmanager
-
-try:
-    # Python 3.6+
-    from os import PathLike
-except ImportError:
-    from pathlib import PurePath as PathLike
 
 
 def is_buffer(obj, mode):
@@ -18,12 +11,6 @@ def is_buffer(obj, mode):
 def open_file(path_or_buf, mode="r"):
     if is_buffer(path_or_buf, mode):
         yield path_or_buf
-    elif sys.version_info < (3, 6) and isinstance(path_or_buf, PathLike):
-        # TODO remove when python 3.5 is EoL (i.e. 2020-09-13)
-        # https://devguide.python.org/#status-of-python-branches
-        # https://www.python.org/dev/peps/pep-0478/
-        with open(str(path_or_buf), mode) as f:
-            yield f
     else:
         with open(path_or_buf, mode) as f:
             yield f
