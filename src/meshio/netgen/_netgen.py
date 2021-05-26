6a2ac430e9449bd44aa6f03c4e6a4448f16e6c41
Matthias Hochsteger <mhochsteger@cerbsim.com>
2021-05-26 16:42:37 +0200

    netgen - some more fixes (vertex elements, el index, dimension)

--- src/meshio/netgen/_netgen.py
+++ src/meshio/netgen/_netgen.py
@@ -111,19 +111,23 @@ def read(filename):
         return read_buffer(f)
 
 
-def _read_cells(f, netgen_cell_type, cells):
+def _read_cells(f, netgen_cell_type, cells, cells_index):
     if netgen_cell_type == "pointelements":
         dim = 0
         nump = 1
         pi0 = 0
+        i_index = 1
     if netgen_cell_type.startswith("edgesegments"):
         dim = 1
         nump = 2
-        pi0 = 1
+        pi0 = 2
+        i_index = 0
     if netgen_cell_type == "surfaceelements":
         dim = 2
+        i_index = 1
     if netgen_cell_type == "volumeelements":
         dim = 3
+        i_index = 0
 
     ncells = int(f.readline())
     tmap = netgen_to_meshio_type[dim]
@@ -131,6 +135,7 @@ def _read_cells(f, netgen_cell_type, cells):
     for i in range(ncells):
         line, is_eof = _fast_forward_over_blank_lines(f)
         data = list(filter(None, line.split(" ")))
+        index = int(data[i_index])
         if dim == 2:
             nump = int(data[4])
             pi0 = 5
@@ -141,24 +146,30 @@ def _read_cells(f, netgen_cell_type, cells):
         pi = list(map(int, data[pi0 : pi0 + nump]))
 
         t = tmap[nump]
-        pmap = netgen_to_meshio_pmap[t]
-        pi = [pi[pmap[i]] for i in range(len(pi))]
         if len(cells) == 0 or t != cells[-1][0]:
             cells.append((t, []))
+            cells_index.append([])
         cells[-1][1].append(pi)
+        cells_index[-1].append(index)
 
 
-def _write_cells(f, block):
+def _write_cells(f, block, index=None):
     if len(block) == 0:
         return
     pmap = np.array(meshio_to_netgen_pmap[block.type])
     dim = _topological_dimension[block.type]
     post_data = []
+    pre_data = []
+    i_index = 0
+    if dim == 0:
+        post_data = [1]
+        i_index = 1
     if dim == 1:
         pre_data = [1, 0]
         post_data = [-1, -1, 0, 0, 1, 0, 1, 0]
     if dim == 2:
         pre_data = [1, 1, 0, 0, len(pmap)]
+        i_index = 1
     if dim == 3:
         pre_data = [1, len(pmap)]
 
@@ -170,6 +181,8 @@ def _write_cells(f, block):
     pi[:, :col1] = np.repeat([pre_data], len(block), axis=0)
     pi[:, col1:col2] = block.data[:, pmap] + 1
     pi[:, col2:] = np.repeat([post_data], len(block), axis=0)
+    if index is not None:
+        pi[:, i_index] = index
     np.savetxt(f, pi, "%i")
 
 
@@ -182,6 +195,7 @@ def _skip_block(f):
 def read_buffer(f):
     points = []
     cells = []
+    cells_index = []
 
     while True:
         # fast-forward over blank lines
@@ -193,7 +207,7 @@ def read_buffer(f):
         if line.startswith("#"):
             continue
         elif line == "dimension":
-            f.readline()
+            dimension = int(f.readline())
         elif line == "geomtype":
             f.readline()
         elif line == "mesh3d":
@@ -202,14 +216,18 @@ def read_buffer(f):
             npoints = int(f.readline())
             if npoints > 0:
                 points = np.fromfile(f, sep=" ", count=3 * npoints).reshape(npoints, 3)
+                if dimension == 2:
+                    points = points[:, :2]
 
         elif line in [
             "pointelements",
             "edgesegments",
+            "edgesegmentsgi",
+            "edgesegmentsgi2",
             "surfaceelements",
             "volumeelements",
         ]:
-            _read_cells(f, line, cells)
+            _read_cells(f, line, cells, cells_index)
 
         elif line == "endmesh":
             break
@@ -233,39 +251,42 @@ def read_buffer(f):
             continue
 
     # convert to numpy arrays
-    # Subtract one to account for the fact that python indices are 0-based.
-    for k, c in enumerate(cells):
-        cells[k] = (c[0], np.array(c[1], dtype=int) - 1)
+    # subtract one (netgen counts 1-based)
+    # apply permutation of vertex numbers
+    for k, (t, data) in enumerate(cells):
+        pmap = netgen_to_meshio_pmap[t]
+        d = np.array(data, dtype=np.uint32)
+        d[:, :] = d[:, pmap] - 1
+        cells[k] = (t, d)
 
-    # Construct the mesh object
-    mesh = Mesh(points, cells)
+    mesh = Mesh(points, cells, cell_data={"index": cells_index})
     return mesh
 
 
-def write(filename, mesh):
-    point_format = "fixed-large"
-    cell_format = "fixed-small"
+def write(filename, mesh, float_fmt=".16e"):
     if str(filename).endswith(".vol.gz"):
         import gzip
 
         with gzip.open(filename, "wt") as f:
-            return write_buffer(f, mesh, point_format, cell_format)
+            return write_buffer(f, mesh, float_fmt)
 
     with open_file(filename, "w") as f:
-        return write_buffer(f, mesh, point_format, cell_format)
+        return write_buffer(f, mesh, float_fmt)
 
 
-def write_buffer(f, mesh, point_format, cell_format):
+def write_buffer(f, mesh, float_fmt):
+    npoints, dimension = mesh.points.shape
     cells_per_dim = [0, 0, 0, 0]
+    cells_index = mesh.cell_data['index'] if 'index' in mesh.cell_data else [None] * len(mesh.cells)
     for block in mesh.cells:
         cells_per_dim[_topological_dimension[block.type]] += len(block)
 
     print(
-        """# Generated by meshio {}
+        f"""# Generated by meshio {__version__}
 mesh3d
 
 dimension
-3
+{dimension}
 
 geomtype
 0
@@ -277,16 +298,16 @@ geomtype
     print("\n# surfnr    bcnr   domin  domout      np      p1      p2      p3", file=f)
     print("surfaceelements", file=f)
     print(cells_per_dim[2], file=f)
-    for block in mesh.cells:
+    for block, index in zip(mesh.cells, cells_index):
         if _topological_dimension[block.type] == 2:
-            _write_cells(f, block)
+            _write_cells(f, block, index)
 
     print("\n#  matnr      np      p1      p2      p3      p4", file=f)
     print("volumeelements", file=f)
     print(cells_per_dim[3], file=f)
-    for block in mesh.cells:
+    for block, index in zip(mesh.cells, cells_index):
         if _topological_dimension[block.type] == 3:
-            _write_cells(f, block)
+            _write_cells(f, block, index)
 
     print(
         "\n# surfid  0   p1   p2   trignum1    trignum2   domin/surfnr1    domout/surfnr2   ednr1   dist1   ednr2   dist2",
@@ -294,44 +315,28 @@ geomtype
     )
     print("edgesegmentsgi2", file=f)
     print(cells_per_dim[1], file=f)
-    for block in mesh.cells:
+    for block, index in zip(mesh.cells, cells_index):
         if _topological_dimension[block.type] == 1:
-            _write_cells(f, block)
+            _write_cells(f, block, index)
 
     print("\n#          X             Y             Z", file=f)
     print("points", file=f)
     print(len(mesh.points), file=f)
 
     points = mesh.points
-    npoints, dim = points.shape
-    if dim == 2:
+    if dimension == 2:
         p = np.zeros((points.shape[0], 3), dtype=points.dtype)
         p[:, :2] = points
     else:
         p = points
-    np.savetxt(f, p, "% 20.16f")
+    np.savetxt(f, p, "%" + float_fmt)
 
     print("\n#          pnum             index", file=f)
     print("pointelements", file=f)
-    print("0", file=f)
-
-    print(
-        """
-materials
-1
-1 domain
-
-bcnames
-1
-1 boundary
-
-#   Surfnr     Red     Green     Blue
-face_colours
-1
-       1   0.00000000   1.00000000   0.00000000
-""",
-        file=f,
-    )
+    print(cells_per_dim[0], file=f)
+    for block, index in zip(mesh.cells, cells_index):
+        if _topological_dimension[block.type] == 0:
+            _write_cells(f, block, index)
 
     print("endmesh", file=f)
 
