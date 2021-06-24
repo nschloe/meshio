a55bba56c59feb493fe334fa1c1963fe89f480ff
Stef Smeets <s.smeets@esciencecenter.nl>
2021-06-24 15:48:28 +0200

    Fix path to vtk module

--- tools/paraview-meshio-plugin.py
+++ tools/paraview-meshio-plugin.py
@@ -12,8 +12,8 @@ from vtkmodules.vtkCommonDataModel import vtkUnstructuredGrid
 import meshio
 
 paraview_plugin_version = meshio.__version__
-vtk_to_meshio_type = meshio.vtk._vtk.vtk_to_meshio_type
-meshio_to_vtk_type = meshio.vtk._vtk.meshio_to_vtk_type
+vtk_to_meshio_type = meshio.vtk_42._vtk.vtk_to_meshio_type
+meshio_to_vtk_type = meshio.vtk_42._vtk.meshio_to_vtk_type
 meshio_input_filetypes = list(meshio._helpers.reader_map.keys())
 meshio_extensions = [ext[1:] for ext in meshio.extension_to_filetype.keys()]
 meshio_input_filetypes = ["automatic"] + meshio_input_filetypes
